0213f4438fb750b444fcbc13b40830408b038048
jaylogical <jay.logicalstreet@gmail.com>
2024-06-11 10:21:04 +0530

    Add Omniauth gem and related code

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem "pg", "~> 1.1"
 
 gem "devise"
 
+gem "omniauth"
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
   resources :oauth_clients
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, sign_out_via: [:get, :delete]
 
-  root "dashboards#index"
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # SSO Provider routes
+  match '/auth/sso/authorize' => 'auth#authorize', via: :all
+  match '/auth/sso/access_token' => 'auth#access_token', via: :all
+  match '/auth/sso/user' => 'auth#user', via: :all
+  match '/oauth/token' => 'auth#access_token', via: :all
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "dashboard#index"
 end
